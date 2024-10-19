3e2101c8fed1bbb7df2129d779a5aaed3da427ef
David Adamson <david.adamson2304@gmail.com>
2024-10-19 14:08:38 +0100

    fix: performance under load

--- runtime/src/index.ts
+++ runtime/src/index.ts
@@ -1,31 +1,20 @@
 import { serveDir, serveFile } from "./deps.ts";
 import { download } from "./download.ts";
 
-type Function = {
-  name: string;
-  fetch: (req: Request) => Promise<Response>;
-};
+// TODO
+console.log(Deno.networkInterfaces());
 
 type Deployment = {
   name: string;
   root: string;
-  functions: Map<string, Function>;
+  functions: Set<string>;
 };
 
 const deployments = new Map<string, Deployment>();
 
 // TODO: do not manually create deployments
 const root = await download("tapw");
-// deno-lint-ignore ban-ts-comment
-// @ts-ignore
-const worker = await EdgeRuntime.userWorkers.create({
-  servicePath: `${root}/functions/api`,
-  forceCreate: true,
-  memoryLimitMb: 128,
-  workerTimeoutMs: 15_000,
-});
-const functions = new Map();
-functions.set("api", { name: "api", fetch: (req: Request) => worker.fetch(req) });
+const functions = new Set<string>(["api"]);
 deployments.set("tapw", { name: "tapw", root, functions });
 
 Deno.serve(async (req: Request) => {
@@ -35,12 +24,19 @@ Deno.serve(async (req: Request) => {
   const deployment = deployments.get(url.hostname.split(".")[0]);
   if (!deployment) return new Response("Not Found", { status: 404 });
 
-  const fn = deployment.functions.get(url.pathname.split("/")[1]);
-  if (fn) {
+  const functionName = url.pathname.split("/")[1];
+  if (deployment.functions.has(functionName)) {
     try {
+      // deno-lint-ignore ban-ts-comment
+      // @ts-ignore
+      const fn = await EdgeRuntime.userWorkers.create({
+        servicePath: `${root}/functions/${functionName}`,
+        memoryLimitMb: 128,
+        workerTimeoutMs: 15_000,
+      });
       return fn.fetch(req);
     } catch (e) {
-      console.error(`handling request for function ${deployment.name}.${fn.name} failed`, e);
+      console.error(`handling request for function ${deployment.name}.${functionName} failed`, e);
       return new Response("Internal Server Error", { status: 500 });
     }
   }
