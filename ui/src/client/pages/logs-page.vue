c1215fdc3da43160a721cd1a99fa450b92870ad0
David Adamson <david.adamson2304@gmail.com>
2024-12-07 18:18:43 +0100

    feat: display logs

--- ui/src/client/pages/logs-page.vue
+++ ui/src/client/pages/logs-page.vue
@@ -7,6 +7,7 @@ const server = useServer();
 const route = useRoute();
 
 const logs = ref();
+const l = ref();
 
 watchEffect(async () => {
   const response = await server.getLogs({ params: { deployment: route.params.deployment.toString() } });
@@ -17,8 +18,27 @@ watchEffect(async () => {
 
 <template>
   <h1>Logs for {{ route.params.deployment }}:</h1>
-  <div v-for="log of logs">
-    {{ log }}
+  <table class="w-full table-auto">
+    <tr v-for="(log, i) of logs" :key="i" @click="l = log" class="cursor-pointer">
+      <td>{{ log.level }}</td>
+      <td>{{ log.timestamp }}</td>
+      <td>{{ log.status }}</td>
+      <td>{{ log.type }}</td>
+      <td>{{ log.method }}</td>
+      <td>{{ log.path }}</td>
+      <td v-if="log.logs">{{ log.logs.at(-1)?.message.substring(0, 32) }} ({{ log.logs.length }})</td>
+      <td v-else></td>
+      <td v-if="log.duration">{{ log.duration }}ms</td>
+    </tr>
+  </table>
+  <div v-if="l" class="py-12">
+    <h2>Log details for request: {{ l.requestId }}</h2>
+    <table class="w-full table-auto">
+      <tr v-for="(log, i) in l.logs" :key="i">
+        <td>{{ log.level }}</td>
+        <td>{{ log.timestamp }}</td>
+        <td>{{ log.message }}</td>
+      </tr>
+    </table>
   </div>
-  <pre class="font-mono">{{ JSON.stringify(logs, null, 2) }}</pre>
 </template>
