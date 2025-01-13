cbcc11abc0fd5566055d113bde987483fa25ea98
David Adamson <david.adamson2304@gmail.com>
2025-01-13 22:18:42 +0100

    use button

--- ui/src/client/pages/home-page.vue
+++ ui/src/client/pages/home-page.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+import { Button } from "@/client/components/ui/button";
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
   <h1>nano edge</h1>
-  <router-link to="/deployments/tapw/logs" class="text-primary text-sm hover:underline">
-    click here to see logs of tapw
-  </router-link>
+  <Button :as="RouterLink" to="/deployments/tapw/logs">click here to see logs of tapw</Button>
 </template>
