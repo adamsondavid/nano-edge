bf41a2ca470c90b673944e073867d24e6e81ac45
David Adamson <david.adamson2304@gmail.com>
2025-01-15 21:15:35 +0100

    feat: logs

--- ui/src/client/pages/logs-page.vue
+++ ui/src/client/pages/logs-page.vue
@@ -8,27 +8,30 @@ import alertTriangle from "@iconify-icons/lucide/alert-triangle";
 import functionSquare from "@iconify-icons/lucide/function-square";
 import database from "@iconify-icons/lucide/database";
 import { cva } from "class-variance-authority";
+import { AccordionContent, AccordionItem, AccordionRoot, AccordionTrigger } from "radix-vue";
+import { Request } from "@/common/contract";
+import { cn } from "@/client/lib/utils";
 
 const server = useServer();
 const route = useRoute();
 
-const logs = ref();
-const l = ref();
+const requests = ref([] as Request[]);
+const selectedRequestId = ref<string>();
 
 watchEffect(async () => {
+  requests.value = [];
   const response = await server.getLogs({ params: { deployment: route.params.deployment.toString() } });
-  if (response.status === 200) logs.value = response.body;
-  else logs.value = "some error";
+  if (response.status === 200) requests.value = response.body.reverse();
 });
 
-const buttonVariants = cva(
-  "w-full rounded-sm m-0.5 h-7 font-mono text-sm text-nowrap flex items-center gap-8 text-left select-text px-2",
+const variants = cva(
+  "w-full rounded-sm mb-0.5 min-h-7 max-h-fit font-mono text-sm text-nowrap flex py-1 gap-8 text-left select-text px-2",
   {
     variants: {
       logLevel: {
         info: "even:bg-gray-100 hover:bg-gray-200",
-        warn: "bg-yellow-100 hover:bg-yellow-200",
-        error: "bg-red-100 hover:bg-red-200",
+        warn: "bg-yellow-50 hover:bg-yellow-100 text-yellow-700",
+        error: "bg-red-50 hover:bg-red-100 text-red-700",
       },
     },
   },
@@ -52,63 +55,87 @@ function formatTime(date: Date) {
 const formatMilliseconds = (date: Date) => `.${date.getMilliseconds().toString().padStart(4, "0")}`;
 
 function mapStatusCodeColor(status: number) {
-  if (status < 300) return "text-green-600";
-  else if (status < 400) return "text-yellow-600";
-  else return "text-red-600";
+  if (status < 300) return "text-green-700";
+  else if (status < 400) return "text-yellow-700";
+  else return "text-red-700";
 }
 </script>
 
 <template>
-  <div class="p-1">
-    <button
-      v-for="(log, i) of logs"
-      :key="i"
-      :class="buttonVariants({ logLevel: getMostSignificantLogLevel(log) })"
-      @click="l = log"
-    >
-      <span>
-        <Icon v-if="getMostSignificantLogLevel(log) === 'info'" :icon="info" width="16" class="text-muted-foreground" />
-        <Icon
-          v-else-if="getMostSignificantLogLevel(log) === 'warn'"
-          :icon="alertTriangle"
-          width="16"
-          class="text-yellow-600"
-        />
-        <Icon
-          v-else-if="getMostSignificantLogLevel(log) === 'error'"
-          :icon="alertTriangle"
-          width="16"
-          class="text-red-600"
-        />
-      </span>
-      <span>
-        <span class="text-muted-foreground">{{ formatDate(new Date(log.timestamp)) }}</span>
-        <b>{{ formatTime(new Date(log.timestamp)) }}</b>
-        <span class="text-muted-foreground">{{ formatMilliseconds(new Date(log.timestamp)) }}</span>
-      </span>
-      <span>{{ log.requestId }}</span>
-      <span :class="mapStatusCodeColor(log.status)">{{ log.status }}</span>
-      <span class="flex items-center gap-2">
-        <Icon v-if="log.type === 'INBOUND_FUNCTION_REQUEST'" :icon="functionSquare" width="16" />
-        <Icon v-else-if="log.type === 'INBOUND_STATIC_REQUEST'" :icon="database" width="16" />
-        <span class="min-w-[40em] max-w-[40em] truncate">
-          <span class="text-muted-foreground">{{ log.method + " " }}</span>
-          <span :title="log.path">{{ log.path }}</span>
+  <accordion-root v-model="selectedRequestId" type="single" collapsible class="p-2">
+    <accordion-item v-for="request in requests" :key="request.requestId" :value="request.requestId" as-child>
+      <!-- TODO: extract component -->
+      <accordion-trigger :class="variants({ logLevel: getMostSignificantLogLevel(request) })">
+        <span>
+          <Icon v-if="getMostSignificantLogLevel(request) === 'info'" :icon="info" width="16" />
+          <Icon v-if="getMostSignificantLogLevel(request) === 'warn'" :icon="alertTriangle" width="16" />
+          <Icon v-else-if="getMostSignificantLogLevel(request) === 'error'" :icon="alertTriangle" width="16" />
         </span>
-      </span>
-      <span v-if="log.logs" class="truncate">{{ log.logs.at(-1).message }}</span>
-      <span v-if="log.logs" class="text-muted-foreground">({{ log.logs.length - 1 }} more)</span>
-      <span v-if="log.duration !== undefined" class="ml-auto">{{ log.duration }}ms</span>
-    </button>
-  </div>
-  <div v-if="l" class="py-12">
-    <h2>Log details for request: {{ l.requestId }}</h2>
-    <table class="w-full table-auto">
-      <tr v-for="(log, i) in l.logs" :key="i">
-        <td>{{ log.level }}</td>
-        <td>{{ log.timestamp }}</td>
-        <td class="font-mono whitespace-pre">{{ log.message }}</td>
-      </tr>
-    </table>
-  </div>
+        <span>
+          <span class="opacity-60">{{ formatDate(new Date(request.timestamp)) }}</span>
+          <span>{{ formatTime(new Date(request.timestamp)) }}</span>
+          <span class="opacity-60">{{ formatMilliseconds(new Date(request.timestamp)) }}</span>
+        </span>
+        <span class="opacity-60">{{ request.requestId }}</span>
+        <span :class="mapStatusCodeColor(request.status)">{{ request.status }}</span>
+        <span class="flex items-center gap-2">
+          <span class="opacity-60">
+            <Icon v-if="request.type === 'INBOUND_FUNCTION_REQUEST'" :icon="functionSquare" width="16" />
+            <Icon v-else-if="request.type === 'INBOUND_STATIC_REQUEST'" :icon="database" width="16" />
+          </span>
+          <span class="min-w-[25em] max-w-[25em] truncate">
+            <span class="opacity-60">{{ request.method + " " }}</span>
+            <span :title="request.path">{{ request.path }}</span>
+          </span>
+        </span>
+        <span v-if="request.logs" class="truncate">{{ request.logs.at(-1)?.message }}</span>
+        <span v-if="request.logs?.length" class="opacity-60">({{ request.logs.length - 1 }} more)</span>
+        <span v-if="request.duration !== undefined" class="ml-auto">{{ request.duration }}ms</span>
+      </accordion-trigger>
+      <!-- TODO: extract component?? -->
+      <accordion-content v-if="request.logs" class="overflow-hidden content">
+        <div class="ml-12 mb-12 relative">
+          <div class="absolute w-[3px] top-0 bottom-4 left-[-33px] bg-gray-100" />
+          <div v-for="(log, i) of request.logs" :key="i" :class="cn(variants({ logLevel: log.level }), 'relative')">
+            <div
+              class="absolute w-[11px] h-[11px] top-2 bottom-4 left-[-37px] bg-gray-200 rounded-full border-[1px] border-white"
+            />
+            <div>
+              <span class="opacity-60">{{ formatDate(new Date(log.timestamp)) }}</span>
+              <span>{{ formatTime(new Date(log.timestamp)) }}</span>
+              <span class="opacity-60">{{ formatMilliseconds(new Date(log.timestamp)) }}</span>
+            </div>
+            <span class="whitespace-pre"> {{ log.message }}</span>
+          </div>
+        </div>
+      </accordion-content>
+    </accordion-item>
+  </accordion-root>
 </template>
+
+<style scoped>
+.content[data-state="open"] {
+  animation: slide-down 150ms cubic-bezier(0.87, 0, 0.13, 1);
+}
+.content[data-state="closed"] {
+  animation: slide-up 150ms cubic-bezier(0.87, 0, 0.13, 1);
+}
+
+@keyframes slide-down {
+  from {
+    height: 0;
+  }
+  to {
+    height: var(--radix-accordion-content-height);
+  }
+}
+
+@keyframes slide-up {
+  from {
+    height: var(--radix-accordion-content-height);
+  }
+  to {
+    height: 0;
+  }
+}
+</style>
--- ui/src/common/contract.ts
+++ ui/src/common/contract.ts
@@ -1,7 +1,30 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
 
-const deploymentSchema = z.string().regex(/^[a-z]{1,12}$/);
+const Deployment = z.string().regex(/^[a-z]{1,12}$/);
+
+const Log = z.object({
+  level: z.enum(["info", "error", "warn"]),
+  timestamp: z.coerce.date(),
+  message: z.string(),
+});
+export type Log = z.infer<typeof Log>;
+
+const Request = z.object({
+  type: z.enum(["INBOUND_STATIC_REQUEST", "INBOUND_FUNCTION_REQUEST"]),
+  level: z.enum(["info", "error", "warn"]),
+  timestamp: z.coerce.date(),
+  requestId: z.string().uuid(),
+  function: z.string().optional(),
+  method: z.enum(["GET", "POST", "PUT", "DELETE", "PATCH"]),
+  host: z.string(),
+  path: z.string(),
+  params: z.record(z.string(), z.any()).optional(),
+  status: z.number(),
+  duration: z.number().optional(),
+  logs: z.array(Log).optional(),
+});
+export type Request = z.infer<typeof Request>;
 
 export const contract = initContract().router(
   {
@@ -9,10 +32,10 @@ export const contract = initContract().router(
       method: "GET",
       path: "/deployments/:deployment/logs",
       pathParams: z.object({
-        deployment: deploymentSchema,
+        deployment: Deployment,
       }),
       responses: {
-        200: z.any(), // TODO better schema,
+        200: z.array(Request),
       },
     },
   },
